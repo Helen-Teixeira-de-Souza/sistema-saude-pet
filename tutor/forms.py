c4f1770c51029167788f53a46aa7ece544801fd3
Helen-Teixeira-de-Souza <helensouza.gbi@gmail.com>
2026-08-26 10:07:56 -0300

    correção

--- tutor/forms.py
+++ tutor/forms.py
@@ -4,4 +4,4 @@ from .models import Tutor
 class TutorForm(forms.ModelForm):
     class Meta:
         model = Tutor
-        fields = ['username', 'first_name', 'last_name', 'email', 'telefone']
+        fields = ['username', 'nome', 'email', 'telefone']
--- tutor/models.py
+++ tutor/models.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import User
 
 
 class Tutor(User):
+    nome = models.CharField('Nome', max_length=100, blank=True, default='')
     telefone = models.CharField('Telefone', max_length=20, blank=True, default='')
 
     class Meta:
