c6c76a9cd5b22607b9b52bc2a4c986c57bc3a0f7
SaraMorais2 <saramoraisrodrigues1@gmail.com>
2026-09-06 17:16:31 -0300

    commit

--- mysite/urls.py
+++ mysite/urls.py
@@ -8,11 +8,15 @@ urlpatterns = [
     path('admin/', admin.site.urls),
 <<<<<<< HEAD
     path('pet/', include('pet.urls')),
+
+
+    
     path('vacinacao/', include('vacinacao.urls')),
     path('vacina/', include('vacina.urls')),
     path('cirurgia/', include('cirurgia.urls')),
     path('medicamento/', include('medicamento.urls')),
 =======
+
     path('consultas/', include('consulta.urls')),
 >>>>>>> 25fb0ba02f8af4adb3414067a2d3773ccd0f07d1
 ]
